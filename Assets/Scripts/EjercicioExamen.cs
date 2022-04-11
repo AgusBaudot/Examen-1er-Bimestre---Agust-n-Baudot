2333325a930f78bdc6cf4875200a1ff48c5f0b38
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2022-04-11 13:55:03 -0300

    Completed exercise 1

--- Assets/Scripts/EjercicioExamen.cs
+++ Assets/Scripts/EjercicioExamen.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class EjercicioExamen : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int monto;
+    public string moneda;
     void Start()
     {
-        
+        if (moneda != "D" && moneda != "R" && moneda != "E")
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+        } else if (monto < 1000)
+        {
+            Debug.Log("El monto mínimo es 1000");
+        } else if (moneda == "D")
+        {
+            float convert = monto / 110;
+            Debug.Log(monto + " pesos argentinos equivalen a " + convert + " dólares.");
+        } else if (moneda == "R")
+        {
+            float convert = monto / 25;
+            Debug.Log(monto + " pesos argentinos equivalen a " + convert + " reales.");
+        } else if (moneda == "E")
+        {
+            float convert = monto / 120;
+            Debug.Log(monto + " pesos argentinos equivalen a " + convert + " euros.");
+        }
+
+
     }
 
     // Update is called once per frame
